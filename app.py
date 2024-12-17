23145fbd2847afc0e57c8b98656f578aa83e2fc3
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-12-17 13:47:23 +0530

    added status codes, methods and curated responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,make_response
 
 app = Flask(__name__)
 
@@ -6,13 +6,28 @@ app = Flask(__name__)
 # static
 @app.route('/')
 def index():
-    print("hello world")
     return "<h1>My first flask app</h1>"
 
 # static
-@app.route('/hello')
+# methods by default is get but you can specify only post or only get or both
+# types of methods: GET to get info, POST to submit info, PUT to update info, DELETE to delete info
+# status codes : 200 for assuring that everything worked fine, 300 for redirection, 400 for errors, 500 for server errors
+@app.route('/hello', methods=['GET','POST','PUT','DELETE'])
 def hello():
-    return "Hello World"
+    # response = make_response("hello world")
+    # response.status_code = 202
+    # response.headers['content-type'] = 'text/plain'
+    # return response
+
+    if request.method == 'GET':
+        return '\nYou made a GET request'
+    elif request.method == 'POST':
+        return '\nYou made a POST request',201
+    elif request.method == 'PUT':
+        return '\nYou made a PUT request'
+    else:
+        return '\nYou made a DELETE request'
+    
 
 # url processors = dynamic urls
 @app.route('/greet/<name>')
@@ -36,5 +51,5 @@ def handle_params():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5555, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True,use_reloader =True)
     # debug =false while its been deployed
